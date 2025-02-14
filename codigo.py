d7842861401c63991f2b62a67af7977df029882a
juan10-7 <juansantana1@colegioguanenta.edu.co>
2025-02-14 07:09:52 -0500

    se agregaron nuevoss archivos

---
+++ codigo.py
@@ -0,0 +1,46 @@
+
+# input
+
+h=int(input("ingrese la altura: "))
+
+# processing
+
+q=h/5
+
+n=0
+
+while h>= q:
+
+    h = 0.9 * h
+
+    n = n + 1
+
+# output
+
+print("la cantidad de rebotes fue : " +str(n))
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
